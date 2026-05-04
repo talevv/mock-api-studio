dd1f3748197b189c0b720b58c652652f277a7ee0
Talevv <talevv@proton.me>
2026-05-04 23:17:03 +0200

    Switch to typeorm beta version

--- src/db/data-source.ts
+++ src/db/data-source.ts
@@ -1,5 +1,4 @@
 
-import path from "path";
 import { DataSource } from "typeorm";
 import { Endpoint } from "../models/endpoint.model";
 import { getDbPath } from "../helpers/helpers";
@@ -9,7 +8,7 @@ export const AppDataSource = new DataSource({
   type: "better-sqlite3",
   database: getDbPath(),
   synchronize: false,
-  logging: true,
+  logging: false,
   entities: [Endpoint],
   migrations: [__dirname + "/migrations/*.{ts,js}"],
   subscribers: [],
